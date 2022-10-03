bf6303cca28bf5553e67fe635c321706a9cb6696
DonCosme <109149406+franema@users.noreply.github.com>
2022-10-03 18:22:21 -0300

    Solves indentation

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const playerFactory = (selector, name) => {
 const gameboard = (() => {
     const $gameboardBoxes = document.querySelectorAll(".board-box")
     const selectedBoxes = []
-    return {$gameboardBoxes, selectedBoxes}
+    return { $gameboardBoxes, selectedBoxes }
 })()
 
 const manageGame = (() => {
@@ -15,17 +15,13 @@ const manageGame = (() => {
     const player1 = playerFactory("X", "Player 1")
     const player2 = playerFactory("O", "Player 2")
     let player = player1
-    
-    const getPlayer = () => player
 
+    const getPlayer = () => player
 
     const $restartButton = document.querySelector(".restart-button")
 
     //bind events
-   
     $restartButton.addEventListener("click", start)
-
-    
 
     function start() {
         gameboard.$gameboardBoxes.forEach((box) => {
@@ -37,29 +33,28 @@ const manageGame = (() => {
     }
 
     function threeInRow(selected) {
-        const row1 = selected.filter((box) => (box.id === "1"|| box.id === "2" || box.id === "3"))
-        const row2 = selected.filter((box) => (box.id === "4"|| box.id === "5" || box.id === "6"))
-        const row3 = selected.filter((box) => (box.id === "7"|| box.id === "8" || box.id === "9"))
+        const row1 = selected.filter((box) => (box.id === "1" || box.id === "2" || box.id === "3"))
+        const row2 = selected.filter((box) => (box.id === "4" || box.id === "5" || box.id === "6"))
+        const row3 = selected.filter((box) => (box.id === "7" || box.id === "8" || box.id === "9"))
         return row1.length === 3 || row2.length === 3 || row3.length === 3
     }
 
     function threeInColumn(selected) {
-        const column1 = selected.filter((box) => (box.id === "1"|| box.id === "4" || box.id === "7"))
-        const column2 = selected.filter((box) => (box.id === "2"|| box.id === "5" || box.id === "8"))
-        const column3 = selected.filter((box) => (box.id === "3"|| box.id === "6" || box.id === "9"))
+        const column1 = selected.filter((box) => (box.id === "1" || box.id === "4" || box.id === "7"))
+        const column2 = selected.filter((box) => (box.id === "2" || box.id === "5" || box.id === "8"))
+        const column3 = selected.filter((box) => (box.id === "3" || box.id === "6" || box.id === "9"))
         return column1.length === 3 || column2.length === 3 || column3.length === 3
     }
 
     function threeInDiagonal(selected) {
-        const diagonal1 = selected.filter((box) => (box.id === "1"|| box.id === "5" || box.id === "9"))
-        const diagonal2 = selected.filter((box) => (box.id === "3"|| box.id === "5" || box.id === "7"))
+        const diagonal1 = selected.filter((box) => (box.id === "1" || box.id === "5" || box.id === "9"))
+        const diagonal2 = selected.filter((box) => (box.id === "3" || box.id === "5" || box.id === "7"))
         return diagonal1.length === 3 || diagonal2.length === 3
-            
     }
-    
-    function checkIfWin () {
+
+    function checkIfWin() {
         const selected = gameboard.selectedBoxes.filter(box => box.textContent === player.getSelector())
-        if(threeInRow(selected) || threeInColumn(selected) || threeInDiagonal(selected)) {
+        if (threeInRow(selected) || threeInColumn(selected) || threeInDiagonal(selected)) {
             gameboard.$gameboardBoxes.forEach((box) => {
                 box.classList.add("disabled")
             })
@@ -70,18 +65,16 @@ const manageGame = (() => {
             player = player === player1 ? player2 : player1
             manageDisplay.showPlayersTurn()
         }
-                  
 
     }
 
-    return {getPlayer, checkIfWin}
+    return { getPlayer, checkIfWin }
 })()
 
 
 
 const manageDisplay = (() => {
     const $displayPlayer = document.querySelector(".player")
-
 
     //bind events
     gameboard.$gameboardBoxes.forEach((box) => {
@@ -91,30 +84,28 @@ const manageDisplay = (() => {
     function showInput(e) {
         if (!e.target.classList.value.includes("disabled")) {
             e.target.textContent = manageGame.getPlayer().getSelector()
+            e.target.style.color = "rgb(147, 201, 171)"
             e.target.classList.add("disabled")
             gameboard.selectedBoxes.push(e.target)
             manageGame.checkIfWin()
         }
     }
 
-    function showPlayersTurn () {
+    function showPlayersTurn() {
         $displayPlayer.textContent = `${manageGame.getPlayer().getName()} turn`
     }
 
-    function showWinner () {
+    function showWinner() {
         $displayPlayer.textContent = `${manageGame.getPlayer().getName()} wins!!`
     }
 
-    function showDraw () {
+    function showDraw() {
         $displayPlayer.textContent = `It's a draw!!`
     }
-    
+
     showPlayersTurn()
 
-
-
-
-    return {showPlayersTurn, showWinner, showDraw}
+    return { showPlayersTurn, showWinner, showDraw }
 })()
 
 
